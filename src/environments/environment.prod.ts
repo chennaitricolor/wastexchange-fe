3d76b331b436b465a744d96174084d7c871554a7
Subrahmanyam Bhamidipati <bhsubbu.1995@gmail.com>
2019-07-17 16:02:00 +0530

    Feat: Moving baceknd api to data.wasteexchange.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  hostName: "http://ec2-3-15-46-72.us-east-2.compute.amazonaws.com/",
+  hostName: "https://data.indiawasteexchange.com",
   googleMapsApiKey: "AIzaSyDSPR8JtXAR1_vnYJHpVZPSLYr5Y6_fgDs"
 };
