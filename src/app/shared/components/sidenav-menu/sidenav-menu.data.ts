22cd2a67e402438e82a5841f335c3f6a27088941
spidey <mahesh.g@live.com>
2019-07-28 17:43:40 +0530

    Added advisory team page

--- src/app/shared/components/sidenav-menu/sidenav-menu.data.ts
+++ src/app/shared/components/sidenav-menu/sidenav-menu.data.ts
@@ -1,6 +1,7 @@
 import { Menu } from './sidenav-menu';
 
 export const MENUMASTER: Menu[] = [
+<<<<<<< HEAD
   {
     id: 1,
     link: '',
@@ -38,7 +39,7 @@ export const MENUMASTER: Menu[] = [
       },
       {
         id: 10,
-        link: '',
+        link: "info/advisory-team",
         name: 'Advisory team',
         parentId: 2,
         icon: 'folder_special'
@@ -88,3 +89,92 @@ export const MENUMASTER: Menu[] = [
     icon: 'forum'
   }
 ];
+=======
+    {
+        id: 1,
+        link: "",
+        name: "Home",
+        parentId: 0,
+        icon: "home"
+    },
+    {
+        id: 2,
+        link: "",
+        name: "About us",
+        parentId: 0,
+        icon: "info",
+        children: [
+            {
+                id: 7,
+                link: "info/our-team",
+                name: "Our team",
+                parentId: 2,
+                icon: "people"
+            },
+            {
+                id: 8,
+                link: "info/special-thanks",
+                name: "Special thanks",
+                parentId: 2,
+                icon: "tag_faces"
+            },
+            {
+                id: 9,
+                link: "info/idea",
+                name: "Our idea",
+                parentId: 2,
+                icon: "movie_filter"
+            },
+            {
+                id: 10,
+                link: "info/advisory-team",
+                name: "Advisory team",
+                parentId: 2,
+                icon: "folder_special"
+            },
+            {
+                id: 11,
+                link: "",
+                name: "Resource partners",
+                parentId: 2,
+                icon: "folder_shared"
+            },
+            {
+                id: 12,
+                link: "",
+                name: "Social mandate",
+                parentId: 2,
+                icon: "share"
+            }
+        ]
+    },
+    {
+        id: 3,
+        link: "",
+        name: "How it works",
+        parentId: 0,
+        icon: "help"
+    },
+    {
+        id: 4,
+        link: "",
+        name: "Acts and rules",
+        parentId: 0,
+        icon: "flare"
+    },
+    {
+        id: 5,
+        link: "",
+        name: "Media",
+        parentId: 0,
+        icon: "photo_album"
+    },
+    {
+        id: 6,
+        link: "",
+        name: "Contact us",
+        parentId: 0,
+        icon: "forum"
+    }
+];
+>>>>>>> aff5df6... Added advisory team page
