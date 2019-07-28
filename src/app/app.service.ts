7529ca533174cc46bc4ffed81158dedb67b57b14
spidey <mahesh.g@live.com>
2019-07-28 23:47:24 +0530

    Resolved merge issues

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -20,7 +20,7 @@ export class AppService {
   public allUsers: any[] = [];
   public isLoading: boolean = false;
   public materials = MATERIALS;
-  public title = "INDIA WASTE EXCHANGE";
+  public title = 'INDIA WASTE EXCHANGE';
   public hidePageActions: boolean = false;
 
   constructor(
