3d4fd2e66b3f65b1268bff7df9e8878c953fe4dd
Arunvel Sriram <arunvelsriram@gmail.com>
2019-08-13 01:23:07 +0530

    Add staging app configuration

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  hostName: 'http://ec2-3-15-173-123.us-east-2.compute.amazonaws.com/api',
+  googleMapsApiKey: 'AIzaSyB8NCphJh5-XfQFXb2EcrKO922PgoLt-Aw'
+};
