fbedb40a9edca300127c228abcd2b88dd8bdce39
Arunvel Sriram <arunvelsriram@gmail.com>
2019-09-01 10:57:28 +0530

    Change staging url and update nginx config

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  hostName: 'https://staging.indiawasteexchange.com/api',
+  hostName: 'https://staging-env.indiawasteexchange.com/api',
   googleMapsApiKey: 'AIzaSyBF5139v5xWhHnZVAQSUxY9WLcwASxh3GA'
 };
